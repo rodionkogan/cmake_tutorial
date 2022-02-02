f2a0d008dcbbf3bd71e17885bc0f311ceb65fe02
Rodion Kogan <rodion.kogan@fastdev.se>
2022-02-02 20:01:08 +0300

    Add step-by-step guide for ex1 as comment in main.cpp

--- ex1/main.cpp
+++ ex1/main.cpp
@@ -7,3 +7,25 @@ int main()
 {
     std::cout << "Hello World!!!\n";
 }
+
+/*
+Step 1. Create simple main.cpp file as follows:
+    touch main.cpp
+Step 2. Create simple CMakeLists.txt file as follows:
+    touch CMakeLists.txt
+Step 3. Create out/build directory as follows:
+    mkdir -p out/build/
+Step 4. Go to out/build directory as follows:
+    cd out/build/
+Step 5. Run CMake from out/build directory as follows:
+    cmake -S ../../ -B .
+Step 6. Create .gitignore file as follows:
+    touch .gitignore
+Step 7. Add to .gitignore file following line:
+    out/
+Step 8. Create initial commit
+Step 9. Run Makefile from out/build directory as follows:
+    make
+Step 10. Run app from out/build directory as follows:
+    ./ex1
+*/
