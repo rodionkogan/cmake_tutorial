a20a34b4391379446e30eb9baec50a007523219b
Rodion Kogan <rodion.kogan@fastdev.se>
2022-02-02 21:02:00 +0300

    Add main.cpp [ex3]

---
+++ ex3/main.cpp
@@ -0,0 +1,16 @@
+// CMake Tutorial EP 3 | Git Submodules (adding glfw windowing library example)
+// https://www.youtube.com/watch?v=ED-WUk440qc
+
+#include <iostream>
+
+#include "adder.h"
+
+int main()
+{
+    std::cout <<
+        "CMake\n\n"
+        "Episode 3\n\n"
+        "Git Submodules (adding glfw windowing library example)\n\n";
+
+    std::cout << "Number PI is " << add(1.05, 2.09) << "\n\n";
+}
