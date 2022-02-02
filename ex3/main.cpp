7bca43520bcdb670298b9558321eae15a4cf653e
Rodion Kogan <rodion.kogan@fastdev.se>
2022-02-02 22:07:49 +0300

    Add code snippet to main.cpp to show glfw library usage [ex3]

--- ex3/main.cpp
+++ ex3/main.cpp
@@ -2,7 +2,7 @@
 // https://www.youtube.com/watch?v=ED-WUk440qc
 
 #include <iostream>
-
+#include <GLFW/glfw3.h>
 #include "adder.h"
 
 int main()
@@ -13,4 +13,28 @@ int main()
         "Git Submodules (adding glfw windowing library example)\n\n";
 
     std::cout << "Number PI is " << add(1.05, 2.09) << "\n\n";
+
+    GLFWwindow * window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 }
