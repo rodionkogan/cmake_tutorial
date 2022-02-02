d0ff2156444fe6e877a2ee25d7b1715742914b4b
Rodion Kogan <rodion.kogan@fastdev.se>
2022-02-02 20:04:05 +0300

    Add main.cpp for ex2

---
+++ ex2/main.cpp
@@ -0,0 +1,14 @@
+// CMake Tutorial EP 2 | Libraries and Subdirectories
+// https://www.youtube.com/watch?v=kEGQKzhciKc
+
+#include <iostream>
+
+int main()
+{
+    std::cout <<
+        "CMake\n\n"
+        "Episode 2\n\n"
+        "-Multiple files\n"
+        "-Libraries\n"
+        "-Subfolders\n\n";
+}
