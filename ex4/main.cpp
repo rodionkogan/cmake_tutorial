d96456e17ab3184b1e56ca480cc59ab3bc0f88a2
Rodion Kogan <rodion.kogan@fastdev.se>
2022-02-02 23:48:00 +0300

    Copy all the stuff from ex3 to ex4

---
+++ ex4/main.cpp
@@ -0,0 +1,40 @@
+// CMake Tutorial EP 4 | Versioning Source Code
+// https://www.youtube.com/watch?v=K3bx7NYSXVk
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include "adder.h"
+
+int main()
+{
+    std::cout <<
+        "CMake\n\n"
+        "Episode 4\n\n"
+        "Versioning Source Code\n\n";
+
+    std::cout << "Number PI is " << add(1.05, 2.09) << "\n\n";
+
+    GLFWwindow * window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
